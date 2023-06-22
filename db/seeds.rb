2d309cf50be3de2606a6928e258768ef4486cd32
AmandaFI <a.ferrari.iaquinta@gmail.com>
2023-06-22 14:56:13 -0300

    Deploy configurations.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 Faker::Config.locale = :en
 
 
-puts 'Generating categories...\n'
+# puts 'Generating categories...\n'
 # categories = 50.times.map do |i|
 #   print '.'
 #   Category.create! name: Faker::Commerce.unique.department
@@ -23,38 +23,41 @@ end
 
 
 # puts categories.map(&:name)
-puts 'Generating users...\n'
-users = 30.times.map do |i| 
-  print '.'
-  User.create! name: Faker::Name.name, email: Faker::Internet.email, password: '12345'
-end
+# puts 'Generating users...\n'
+# users = 30.times.map do |i| 
+#   print '.'
+#   User.create! name: Faker::Name.name, email: Faker::Internet.email, password: '12345'
+# end
+User.create! name: "afi", email: "a@fi.com", password: "12345"
+User.create! name: "teste", email: "teste.com", password: "12345"
 
-fake_urls = ['/wXqWR7dHncNRbxoEGybEy7QTe9h.jpg', '/wXqWR7dHncNRbxoEGybEy7QTe9h.jpg', '/9vaRPXj44Q2meHgt3VVfQufiHOJ.jpg', '/ay7xwXn1G9fzX9TUBlkGA584rGi.jpg', '/fNOH9f1aA7XRTzl1sAOx9iF553Q.jpg', '/oU7Oq2kFAAlGqbU4VoAE36g4hoI.jpg', '/momkKuWburNTqKBF6ez7rvhYVhE.jpg', '/oOleziEempUPu96jkGs0Pj6tKxj.jpg', '/snGwr2gag4Fcgx2OGmH9otl6ofW.jpg', '/hEjK9A9BkNXejFW4tfacVAEHtkn.jpg', '/y5Va1WXDX6nZElVirPrGxf6w99B.jpg', '/Nkgaj3X0W2jHQ1TzHEgWFpN3kJ.jpg', '/sizg1AU8f8JDZX4QIgE4pjUMBvx.jpg', '/yFihWxQcmqcaBR31QM6Y8gT6aYV.jpg', '/ei2qjTiDpgKGuoSEntPCSAIhuvQ.jpg']
 
-puts 'Generating lists...\n'
-lists = 100.times.map do |i|
-  print '.'
-  List.create!(
-    title: Faker::Lorem.sentence, 
-    user: users.sample, 
-    category: categories.sample,
-    draft: Faker::Boolean.boolean,
-    items: (rand(8) + 3).times.map do |j|
-      ListItem.new(
-        external_api_identifier: Faker::IDNumber.valid,
-        rank: j,
-        title: Faker::Movie.title,
-        # metadata: {x: 1, y: 2, z: 3},
-        # image_url: Faker::Internet.url,
-        image_url: fake_urls.sample,
-        details: Faker::Date.between(from: '1920-09-23', to: '2023-09-25'),
-        user_comment: Faker::Lorem.sentence(word_count: 15)
-      )
-    end
-  )
-end
+# fake_urls = ['/wXqWR7dHncNRbxoEGybEy7QTe9h.jpg', '/wXqWR7dHncNRbxoEGybEy7QTe9h.jpg', '/9vaRPXj44Q2meHgt3VVfQufiHOJ.jpg', '/ay7xwXn1G9fzX9TUBlkGA584rGi.jpg', '/fNOH9f1aA7XRTzl1sAOx9iF553Q.jpg', '/oU7Oq2kFAAlGqbU4VoAE36g4hoI.jpg', '/momkKuWburNTqKBF6ez7rvhYVhE.jpg', '/oOleziEempUPu96jkGs0Pj6tKxj.jpg', '/snGwr2gag4Fcgx2OGmH9otl6ofW.jpg', '/hEjK9A9BkNXejFW4tfacVAEHtkn.jpg', '/y5Va1WXDX6nZElVirPrGxf6w99B.jpg', '/Nkgaj3X0W2jHQ1TzHEgWFpN3kJ.jpg', '/sizg1AU8f8JDZX4QIgE4pjUMBvx.jpg', '/yFihWxQcmqcaBR31QM6Y8gT6aYV.jpg', '/ei2qjTiDpgKGuoSEntPCSAIhuvQ.jpg']
 
-puts "\nGenerating followers..."
-users.each do |user|
-  user.followers << (users - [user]).sample(rand(10) + 1)
-end
+# puts 'Generating lists...\n'
+# lists = 100.times.map do |i|
+#   print '.'
+#   List.create!(
+#     title: Faker::Lorem.sentence, 
+#     user: users.sample, 
+#     category: categories.sample,
+#     draft: Faker::Boolean.boolean,
+#     items: (rand(8) + 3).times.map do |j|
+#       ListItem.new(
+#         external_api_identifier: Faker::IDNumber.valid,
+#         rank: j,
+#         title: Faker::Movie.title,
+#         # metadata: {x: 1, y: 2, z: 3},
+#         # image_url: Faker::Internet.url,
+#         image_url: fake_urls.sample,
+#         details: Faker::Date.between(from: '1920-09-23', to: '2023-09-25'),
+#         user_comment: Faker::Lorem.sentence(word_count: 15)
+#       )
+#     end
+#   )
+# end
+
+# puts "\nGenerating followers..."
+# users.each do |user|
+#   user.followers << (users - [user]).sample(rand(10) + 1)
+# end
