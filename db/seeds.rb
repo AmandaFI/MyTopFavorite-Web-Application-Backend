0204e97615245ae113972bb4aff737200b0be0b3
AmandaFI <a.ferrari.iaquinta@gmail.com>
2023-06-22 18:09:04 -0300

    Seeds updated again.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 Faker::Config.locale = :en
 
 
-possible_categories = ["Filmes", "Séries", "Pessoas"]
-categories = possible_categories.map do |i|
-  print '.'
-  Category.create! name: i
-end
+# possible_categories = ["Filmes", "Séries", "Pessoas"]
+# categories = possible_categories.map do |i|
+#   print '.'
+#   Category.create! name: i
+# end
 
 
-User.create! name: "testUser2", email: "test@user2.com.com", password: '12345'
-User.create! name: "testUser3", email: "test@user3.com", password: '12345'
+User.create! name: "testUser5", email: "test@user5.com.com", password: '12345'
+User.create! name: "testUser7", email: "test@user7.com", password: '12345'
