35b6141365ec8a3afcfcb0463feac9a992a54645
AmandaFI <a.ferrari.iaquinta@gmail.com>
2023-06-22 19:15:29 -0300

    Cors to allow any request with credentials.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,12 @@
 #   end
 # end
 
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins 'https://mytopfavoriteinterface.onrender.com'
-    resource '*', headers: :any, methods: :any, credentials: true
-  end
-end
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#   allow do
+#     origins 'https://mytopfavoriteinterface.onrender.com'
+#     resource '*', headers: :any, methods: :any, credentials: true
+#   end
+# end
 
 # Rails.application.config.middleware.insert_before 0, Rack::Cors do
 #   allow do
@@ -19,3 +19,20 @@ end
 #     resource '*', headers: :any, methods: :any, credentials: true
 #   end
 # end
+
+before_filter :set_headers
+
+  private
+
+  def set_headers
+    if request.headers["HTTP_ORIGIN"]
+    # better way check origin
+    # if request.headers["HTTP_ORIGIN"] && /^https?:\/\/(.*)\.some\.site\.com$/i.match(request.headers["HTTP_ORIGIN"])
+      headers['Access-Control-Allow-Origin'] = request.headers["HTTP_ORIGIN"]
+      headers['Access-Control-Expose-Headers'] = 'ETag'
+      headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD'
+      headers['Access-Control-Allow-Headers'] = '*,x-requested-with,Content-Type,If-Modified-Since,If-None-Match,Auth-User-Token'
+      headers['Access-Control-Max-Age'] = '86400'
+      headers['Access-Control-Allow-Credentials'] = 'true'
+    end
+  end
